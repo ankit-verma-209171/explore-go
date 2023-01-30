ce4d3a3dc8d0775321de6a76a2c1b04c4b9fd48c
Ankit Verma <ankitverma209171@gmail.com>
2023-01-30 13:48:06 +0000

    removing element from slice

--- 08slices/main.go
+++ 08slices/main.go
@@ -15,4 +15,14 @@ func main() {
 	nums = append(nums, 1, 2, 3, 4, 5)
 	fmt.Println("Nums are", nums)
 	fmt.Println("Nums slice", nums[:3])
+
+	// deleting element from slice
+	colors := []string{"red", "blue", "yellow", "green"}
+	fmt.Println("Colors", colors)
+
+	fmt.Println("Removing yellow")
+	index := 2
+
+	colors = append(colors[:index], colors[index+1:]...)
+	fmt.Println("Colors now", colors)
 }
